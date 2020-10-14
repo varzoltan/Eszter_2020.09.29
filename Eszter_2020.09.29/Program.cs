f0fa5cbdb42996f9c0f395d80e590a9cfc9195e6
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-10-14 10:20:45 +0200

    10:20

--- Eszter_2020.09.29/Program.cs
+++ Eszter_2020.09.29/Program.cs
@@ -16,13 +16,13 @@ namespace Eszter_2020._09._29
             string mondat = "Olvass be egy mondatot, és írd ki a szavait egymás alá.";
             string[] szavak = mondat.Split(' ');
             int n = 0;
-            while (n<szavak.Length)
+            while (n < szavak.Length)
             {
                 Console.WriteLine(szavak[n]);
                 n++;
             }
 
-            for (int i=0; i<szavak.Length; i++)
+            for (int i = 0; i < szavak.Length; i++)
             {
                 Console.WriteLine($"{szavak[i]} ");
             }
@@ -33,9 +33,43 @@ namespace Eszter_2020._09._29
             {
                 Console.WriteLine(j.ToString());
             }
+            //int egyik = 3;
+            Console.WriteLine($"A függvényben megírt két szám összege: {Ket_szam_osszead()}");
+            Console.Write("Kérem adjon meg egy számot: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Kérem adjon meg egy másik számot: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"A paraméterezett függvény értéke, melyet már a felhasználó ad meg: {Ket_szam_osszead_parameterekkel(a, b)}");
+            Console.WriteLine($"A három szám szorzata:{szamok_osszeszorzasa(a, b, 20)} ");
             Console.ReadKey();
         }
+        //Példa
+        static int Ket_szam_osszead()
+        {
+            int egyik = 3;
+            int masik = 4;
+            //int osszeg = egyik + masik;
+            return egyik + masik;
+        }
 
+        //Ez egy másik függvény, de paramétert is tartalmaz!
+        static int Ket_szam_osszead_parameterekkel(int egyik, int masik)
+        {
+            return egyik + masik;
+        }
+
+        //Írj függvényt, amely három egész számot összeszoroz, majd visszatér az értékével!
+        static long szamok_osszeszorzasa(int egyik, int masik, int harmadik)
+        {
+            return egyik * masik * harmadik;
+        }
+
+        //Fontos szabály(ok) paraméterezett függvény létrehozásakor:
+        //1. szabály: a paraméterben megadott érték, vagy változónak minden esetben meg kell adni a títusát.
+        //2. szabály: akárhány paramétere lehet a függvénynek.
+        //3. szabály: a paramétereket egymástól vesszővel válasszuk el.
+
+        //Függvény
         //1.szabály: az osztályon belűl, de a main-en kívűl hozzuk létre.
         //2. szabály: felépítése
         //-láthatóság:  public /Minden osztályból látható
@@ -45,5 +79,6 @@ namespace Eszter_2020._09._29
         //3. szabály: a függvény visszatérési értékének a típusát kell mgadni.
         //4. szabály: a függvénynek a neve.
         //5. szabály: mindenképpen paramétert tartalmaz, de lehet üres is.
+        //6. szabály: kötelezően tartalmaznia kell a "return" kulcsszót.
     }
 }
